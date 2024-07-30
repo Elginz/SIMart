a581e11860bdfdeff9f6676792ffddefacb3efdc
ellenfaustine <138539940+ellenfaustine@users.noreply.github.com>
2024-07-30 15:15:06 +0800

    Update indexRoute.js
    adding the profile route

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -4,10 +4,12 @@ const router = express.Router();
 
 const authorRouter = require("./author.js");
 const readerRouter = require("./reader.js");
+const profileRouter = require("./profile.js");
 
 // Use router and set its browser URL endpoint prefix
 router.use("/reader", readerRouter);
 router.use("/author", authorRouter);
+router.use("/profile", profileRouter);
 
 // Export module containing the following so external files can access it
 module.exports = router;
