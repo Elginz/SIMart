0a7d4e37bee5ef9d83c56bff2f42757a36a457cc
ellenfaustine <138539940+ellenfaustine@users.noreply.github.com>
2024-07-31 19:29:23 +0800

    Update indexRoute.js

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -2,9 +2,11 @@
 const express = require("express");
 const router = express.Router();
 
+const profileRouter = require("./profile.js");
 const productRoute = require("./product.js");
 
 // Use router and set its browser URL endpoint prefix
+router.use("/profile", profileRouter);
 router.use("/product", productRoute);
 
 // Export module containing the following so external files can access it
