4eefc862543862572bf3e7918efadd8fa4364a90
ellenfaustine <138539940+ellenfaustine@users.noreply.github.com>
2024-09-04 15:28:22 +0700

    Update queries.js

--- backend/routes/queries.js
+++ backend/routes/queries.js
@@ -2,32 +2,36 @@
 
 // Function to retrieve user details by email
 const getUserByEmail = (email) => {
-    const userQuery = "SELECT * FROM users WHERE email = ?";
     return new Promise((resolve, reject) => {
+        // Query the database for a user with the specified email
+        const userQuery = "SELECT * FROM users WHERE email = ?";
         global.db.get(userQuery, [email], (err, user) => err ? reject(err) : resolve(user));
     });
 }
 
 // Function to retrieve user details by id
 const getUserById = (id) => {
-    const userQuery = "SELECT * FROM users WHERE id = ?";
     return new Promise((resolve, reject) => {
+        // Query the database for a user with the specified ID
+        const userQuery = "SELECT * FROM users WHERE id = ?";
         global.db.get(userQuery, [id], (err, user) => err ? reject(err) : resolve(user));
     });
 }
 
 // Function to retrieve all courses
 const getAllSchools = () => {
-    const schoolsQuery = "SELECT * FROM courses";
     return new Promise((resolve, reject) => {
+        // Query the database to get all courses
+        const schoolsQuery = "SELECT * FROM courses";
         global.db.all(schoolsQuery, [], (err, schools) => err ? reject(err) : resolve(schools));
     });
 }
 
 // Function to retrieve listings by user ID
 const getListings = (userId) => {
-    const listingsQuery = "SELECT * FROM product WHERE user_id = ? AND offer_status = 'not made'";
     return new Promise((resolve, reject) => {
+        // Query the database for listings belonging to the specified user ID with 'not made' offer status
+        const listingsQuery = "SELECT * FROM product WHERE user_id = ? AND offer_status = 'not made'";
         global.db.all(listingsQuery, [userId], (err, listings) => err ? reject(err) : resolve(listings));
     });
 }
@@ -37,6 +41,7 @@ const getImagesForProducts = (products) => {
     const listingsImages = {};
     const promises = products.map(product => {
         return new Promise((resolve, reject) => {
+            // Query the database for images associated with each product ID
             global.db.all("SELECT * FROM product_images WHERE product_id = ?", [product.id], (err, images) => {
                 if (err) return reject(err);
                 listingsImages[product.id] = images;
@@ -44,13 +49,15 @@ const getImagesForProducts = (products) => {
             });
         });
     });
+    // Wait for all image retrieval promises to resolve
     return Promise.all(promises).then(() => listingsImages);
 }
 
 // Function to retrieve reviews for a user
 const getReviews = (userId) => {
-    const reviewsQuery = "SELECT * FROM reviews WHERE user_id = ?";
     return new Promise((resolve, reject) => {
+        // Query the database for reviews associated with the specified user ID
+        const reviewsQuery = "SELECT * FROM reviews WHERE user_id = ?";
         global.db.all(reviewsQuery, [userId], (err, reviews) => err ? reject(err) : resolve(reviews));
     });
 }
@@ -60,6 +67,7 @@ const getReviewersForReviews = (reviews) => {
     const reviewers = {};
     const promises = reviews.map(review => {
         return new Promise((resolve, reject) => {
+            // Query the database for each reviewer's details using their ID
             const reviewerQuery = "SELECT * FROM users WHERE id = ?";
             global.db.get(reviewerQuery, [review.reviewer_id], (err, reviewer) => {
                 if (err) return reject(err);
@@ -68,38 +76,42 @@ const getReviewersForReviews = (reviews) => {
             });
         });
     });
+    // Wait for all reviewer retrieval promises to resolve
     return Promise.all(promises).then(() => reviewers);
 }
 
 // Function to retrieve transactions for a user
 const getTransactions = (userId) => {
-    const transactionsQuery = "SELECT * FROM product WHERE (user_id = ? OR offer_made_by = ?) AND offer_status != 'not made'";
     return new Promise((resolve, reject) => {
+        // Query the database for transactions involving the user either as the creator or offer maker, excluding 'not made' statuses
+        const transactionsQuery = "SELECT * FROM product WHERE (user_id = ? OR offer_made_by = ?) AND offer_status != 'not made'";
         global.db.all(transactionsQuery, [userId, userId], (err, transactions) => err ? reject(err) : resolve(transactions));
     });
 }
 
 // Function to retrieve favourite products of a user
 const getFavourites = (userId) => {
-    const favouritesQuery = `
-        SELECT product.id, product.name, product.price, product.transaction_type
-        FROM favourites
-        JOIN product ON favourites.product_id = product.id
-        WHERE favourites.user_id = ?
-    `;
     return new Promise((resolve, reject) => {
+        // Query the database for products marked as favourites by the specified user ID
+        const favouritesQuery = `
+            SELECT product.id, product.name, product.price, product.transaction_type
+            FROM favourites
+            JOIN product ON favourites.product_id = product.id
+            WHERE favourites.user_id = ?
+        `;
         global.db.all(favouritesQuery, [userId], (err, favourites) => err ? reject(err) : resolve(favourites));
     });
 }
 
 // Utility function to update user attributes
 const updateUserAttribute = (attribute, value, id, res) => {
-    const query = `UPDATE users SET ${attribute} = ? WHERE id = ?`;
     new Promise((resolve, reject) => {
+        // Update a specific attribute of a user in the database
+        const query = `UPDATE users SET ${attribute} = ? WHERE id = ?`;
         global.db.run(query, [value, id], (err) => err ? reject(err) : resolve());
     })
-    .then(() => res.redirect('/profile'))
-    .catch(err => res.status(500).send(err.message));
+    .then(() => res.redirect('/profile')) // Redirect to the profile page on success
+    .catch(err => res.status(500).send(err.message)); // Send error response on failure
 };
 
 // Function to render transaction type HTML
@@ -111,13 +123,15 @@ const renderTransactionType = (type) => {
     };
     
     const color = colors[type.trim()] || '#CCCCCC'; // Default color if type is not matched
+    // Return HTML for the transaction type with the appropriate background color
     return `<div class="px-4 pt-1 pb-1.5 rounded-full text-white" style="background-color: ${color};">${type.trim()}</div>`;
 };
 
 // Function to retrieve all listings by category
 const getListingsByCategory = (category) => {
-    const query = "SELECT * FROM product WHERE LOWER(category) = ? AND offer_status = 'not made'";
     return new Promise((resolve, reject) => {
+        // Query the database for listings in the specified category with 'not made' offer status
+        const query = "SELECT * FROM product WHERE LOWER(category) = ? AND offer_status = 'not made'";
         global.db.all(query, [category], (err, listings) => err ? reject(err) : resolve(listings));
     });
 };
@@ -125,6 +139,7 @@ const getListingsByCategory = (category) => {
 // Function to retrieve a product by its ID
 const getProductById = (id) => {
     return new Promise((resolve, reject) => {
+        // Query the database for a product by its ID
         const query = "SELECT * FROM product WHERE id = ?";
         db.get(query, [id], (err, row) => err ? reject(err) : resolve(row));
     });
@@ -133,6 +148,7 @@ const getProductById = (id) => {
 // Function to retrieve all images for a specific product
 const getProductImagesById = (productId) => {
     return new Promise((resolve, reject) => {
+        // Query the database for images associated with a specific product ID
         const query = "SELECT * FROM product_images WHERE product_id = ?";
         db.all(query, [productId], (err, rows) => err ? reject(err) : resolve(rows));
     });
@@ -141,6 +157,7 @@ const getProductImagesById = (productId) => {
 // Function to insert a new product into the database
 const insertProduct = (product) => {
     return new Promise((resolve, reject) => {
+        // Insert a new product record into the database
         const query = "INSERT INTO product (user_id, name, description, price, category, transaction_type, condition, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
         db.run(query, [product.userId, product.name, product.description, product.price, product.category, product.transaction_type, product.condition, new Date().toLocaleString()], function (err) {
             if (err) return reject(err);
@@ -152,6 +169,7 @@ const insertProduct = (product) => {
 // Function to insert a new image for a product
 const insertProductImage = (productId, imageData, imageType) => {
     return new Promise((resolve, reject) => {
+        // Insert an image record for a specific product into the database
         const query = "INSERT INTO product_images (product_id, image, image_type) VALUES (?, ?, ?)";
         db.run(query, [productId, imageData, imageType], err => err ? reject(err) : resolve());
     });
@@ -163,6 +181,7 @@ const updateProduct = (id, updates) => {
         const fields = Object.keys(updates).map(key => `${key} = ?`).join(', ');
         const values = Object.values(updates);
         values.push(id);
+        // Update the product record with new values based on the provided updates object
         const query = `UPDATE product SET ${fields} WHERE id = ?`;
         db.run(query, values, err => err ? reject(err) : resolve());
     });
@@ -171,6 +190,7 @@ const updateProduct = (id, updates) => {
 // Function to delete a product from the database
 const deleteProduct = (id) => {
     return new Promise((resolve, reject) => {
+        // Delete a product record from the database using its ID
         const query = "DELETE FROM product WHERE id = ?";
         db.run(query, [id], err => err ? reject(err) : resolve());
     });
@@ -179,6 +199,7 @@ const deleteProduct = (id) => {
 // Function to update the offer status of a product
 const updateOfferMadeBy = (productId, userId) => {
     return new Promise((resolve, reject) => {
+        // Update the 'offer_made_by' field of a product with the specified user ID
         const query = "UPDATE product SET offer_made_by = ? WHERE id = ?";
         db.run(query, [userId, productId], (err) => {
             if (err) return reject(err);
@@ -190,6 +211,7 @@ const updateOfferMadeBy = (productId, userId) => {
 // Function to update the offer status of a product
 const updateOfferStatus = (productId, status) => {
     return new Promise((resolve, reject) => {
+        // Update the 'offer_status' field of a product with the specified user ID
         const query = "UPDATE product SET offer_status = ? WHERE id = ?";
         db.run(query, [status, productId], (err) => {
             if (err) return reject(err);
@@ -201,6 +223,7 @@ const updateOfferStatus = (productId, status) => {
 // Function to insert a new review into the database
 const insertReview = (userId, reviewerId, review, createdAt, rating) => {
     return new Promise((resolve, reject) => {
+        // Insert review data into the 'reviews' table
         const query = "INSERT INTO reviews (user_id, reviewer_id, content, created_at, stars_given) VALUES (?, ?, ?, ?, ?)";
         db.run(query, [userId, reviewerId, review, createdAt, rating], err => err ? reject(err) : resolve());
     });
@@ -209,6 +232,7 @@ const insertReview = (userId, reviewerId, review, createdAt, rating) => {
 // Function to retrieve all reviews for a specific user
 const getReviewsByUserId = (userId) => {
     return new Promise((resolve, reject) => {
+        // Get all reviews for a specific user from the 'reviews' table
         const query = "SELECT * FROM reviews WHERE user_id = ?";
         db.all(query, [userId], (err, rows) => err ? reject(err) : resolve(rows));
     });
@@ -217,6 +241,7 @@ const getReviewsByUserId = (userId) => {
 // Function to update the rating of a user
 const updateUserRating = (userId, rating) => {
     return new Promise((resolve, reject) => {
+        // Update the user's rating in the 'users' table
         const query = "UPDATE users SET rating = ? WHERE id = ?";
         db.run(query, [rating, userId], err => err ? reject(err) : resolve());
     });
@@ -225,6 +250,7 @@ const updateUserRating = (userId, rating) => {
 // Function to retrieve whether a user has favorited a specific product
 const getProductFavourites = (userId, productId) => {
     return new Promise((resolve, reject) => {
+        // Check if a user has favorited a specific product
         const query = "SELECT * FROM favourites WHERE user_id = ? AND product_id = ?";
         db.get(query, [userId, productId], (err, row) => err ? reject(err) : resolve(row));
     });
@@ -233,6 +259,7 @@ const getProductFavourites = (userId, productId) => {
 // Function to add a product to a user's favourites
 const addProductToFavourites = (userId, productId) => {
     return new Promise((resolve, reject) => {
+        // Add a product to the user's favourites
         const query = "INSERT INTO favourites (user_id, product_id) VALUES (?, ?)";
         db.run(query, [userId, productId], err => err ? reject(err) : resolve());
     });
@@ -241,6 +268,7 @@ const addProductToFavourites = (userId, productId) => {
 // Function to remove a product from a user's favourites
 const removeProductFromFavourites = (userId, productId) => {
     return new Promise((resolve, reject) => {
+        // Remove a product from the user's favourites
         const query = "DELETE FROM favourites WHERE user_id = ? AND product_id = ?";
         db.run(query, [userId, productId], err => err ? reject(err) : resolve());
     });
